63f0f309772f5565bfeb1875141b5511fb62bb12
Veronica_Flores <floresnateraveronica@gmail.com>
2024-12-05 20:24:43 +0100

    Normalizar finales de línea

--- plataforma-empresa-frontend/src/App.jsx
+++ plataforma-empresa-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from './Route/Router';
+import AppRouter from './Route/Router.jsx';
 
 const App = () => {
   return (
