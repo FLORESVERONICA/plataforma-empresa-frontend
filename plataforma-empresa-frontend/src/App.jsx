aa9fa1526580238021f6954111b8c37fd44d3ce2
Veronica_Flores <floresnateraveronica@gmail.com>
2024-12-05 20:48:31 +0100

    'tempRoute'

--- plataforma-empresa-frontend/src/App.jsx
+++ plataforma-empresa-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from './route/Router.jsx';
+import AppRouter from './tempRoute/Router.jsx';
 
 const App = () => {
   return (
